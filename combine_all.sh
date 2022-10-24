2dff3e02a93602233bf332085396add90aeb2042
Boiangiu Victor-Miron <miron.boiangiu@gmail.com>
2022-10-25 00:36:42 +0300

    Check imports to keep
    TODO: add commit message (will probably forget to do it).
    
    Signed-off-by: Boiangiu Victor-Miron <miron.boiangiu@gmail.com>

--- combine_all.sh
+++ combine_all.sh
@@ -4,6 +4,8 @@
 
     This is a simple bash script for combining all .java source files from a 
     directory and subdirectories into a single file.
+
+    Usage: run from the root of your project.
 '
 
 shopt -s globstar
@@ -67,40 +69,96 @@ function compute_package_name(){
         fi
     done
 
+
     file_package="$(cat $file_to_consider | grep package | cut -d " " -f2)"
     file_package=${file_package::-1} 
     file_path="$(realpath $file_to_consider)"
     file_path=${file_path%/*}
 
     if [[ $file_path == $my_path ]]; then
-        echo "$file_package"
+        echo "$file_package 1"
     else
         my_path_wc=$(echo $my_path | tr "/" " " | wc -w)
         file_path_wc=$(echo $file_path | tr "/" " " | wc -w)
-        number_of_deletions=$(($file_path_wc - $my_path_wc))        
-        file_package=${file_package/.//}
-        for ((i = 0 ; i < $number_of_deletions ; i++)); do
-            file_package=${file_package%/*}
-        done
-        file_package=${file_package///.}
-        echo "$file_package"
+        number_of_deletions=$(($file_path_wc - $my_path_wc))
+        package_wc=$(echo $file_package | tr "." " " | wc -w)
+        if [[ $package_wc -le number_of_deletions ]]; then
+            echo "NO_PACKAGE 0"
+        else
+            file_package=${file_package//.//}
+            for ((i = 0 ; i < $number_of_deletions ; i++)); do
+                file_package=${file_package%/*}
+            done
+            file_package=${file_package////.}
+            echo "$file_package 1"
+        fi
     fi
 }
 
-function create_single_script(){
-    package=""
+function interpret_package_name_result(){
+    package="$1"
+    pkg_determined="$2"
+    shift 2
+    if [[ $pkg_determined -eq 0 ]]; then
+        echo -e "Empty package name or failed to determine it." \
+        "Please manually check all imports and the package name.\n"
+    else
+        echo -e "New package name seems to be \"$package\"\n"
+        echo -e "package $package;\n" >> single_script.java
+    fi
+}
 
+function parse_imports(){
+    package="$1"
+    pkg_determined="$2"
+    shift 2
+
+    sort -u -o imports.temp imports.temp
+
+    if [[ $pkg_determined -eq 0 ]]; then
+        while IFS= read -r line || [ -n "$line" ]; do
+            echo "$line" >> single_script.java
+        done < "imports.temp"
+    else
+
+        current_dir="$(pwd)"
+        package_wc=$(echo $package | tr "." " " | wc -w)
+        project_root_dir="$current_dir"
+        for ((i = 0 ; i < $package_wc ; i++)); do
+            project_root_dir=${project_root_dir%/*}
+        done
+        echo "Root: $project_root_dir"
+        while IFS= read -r line || [ -n "$line" ]; do
+            if [ -z "$line" ]; then
+                continue
+            fi
+            import_name=$(echo $line | cut -d " " -f2)
+            import_name=${import_name::-1}
+            import_location=$(echo $import_name | tr "." "/")
+            import_location="$project_root_dir/$import_location.java"
+            if ! [ -f "$import_location" ]; then
+                echo "$line" >> single_script.java
+            else
+                echo "Import $import_name found in current project."
+            fi
+        done < "imports.temp"
+    fi
+    echo ""
+}
+
+function create_single_script(){
     echo -e "\nCreating necessary files.\n"
     create_empty_necessary_files
 
     echo -e "Will go through all source files.\n"
     parse_all_script_files
 
-    echo -e "Computing new package name.";
-    package=$(compute_package_name)
-    
-    echo -e "New package name seems to be \"$package\".\n"
-    echo -e "package $package;\n" >> single_script.java
+    echo -e "Computing new package name."
+    read package pkg_determined < <(compute_package_name)
+    interpret_package_name_result $package $pkg_determined
+
+    echo -e "Checking which imports to keep.\n"
+    parse_imports $package $pkg_determined
 
     echo -e "Deleting temporary files.\n"
     cleanup
